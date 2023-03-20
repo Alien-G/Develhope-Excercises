3a3bdb7492859e4a8d317179cc43aefebc599e1c
GA <gaia.addeo@gmail.com>
2023-03-20 20:14:12 +0100

    Added exercise 15

--- src/_16/Main.java
+++ src/_16/Main.java
@@ -50,13 +50,21 @@ public class Main {
             processNames(name);
         }
     }
-    private static String processNames(String name) {
-        if (name.matches(".*\\d.*")) {
+    private static void processNames(String name) {
+        if (name.contains("0") ||
+            name.contains("1") ||
+            name.contains("2") ||
+            name.contains("3") ||
+            name.contains("4") ||
+            name.contains("5") ||
+            name.contains("6") ||
+            name.contains("7") ||
+            name.contains("8") ||
+            name.contains("9")) {
             System.out.println("Invalid");
         } else {
             System.out.println(name.toUpperCase().trim());
         }
-        return name;
     }
 
     /**
